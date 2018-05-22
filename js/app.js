592ca1b5ac92c1a028e05626925d3a03e7a00463
agraham <agraham0925@gmail.com>
2018-05-22 16:13:43 -0500

    updated nav bar style

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 console.log("js file connected");
 
-let teamPersonal = $('<p/>').text("TERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let teamPersonal = $('<p/>').text("I'm an avid ultimate frisbee player. Being apart of the team ELevate for the last 4 years has SOMETHING ABOUT SENSE OF COMMUNITY ETC. Every summer, I get to travel and compete in various midwest tournaments with some of my best friends.")
 teamPersonal.attr('id', "teamPersonal");
 
 let commPersonal = $('<p/>').text("CERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
 commPersonal.attr('id', "commPersonal")
 
-let studentPersonal = $('<p/>').text("SERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let studentPersonal = $('<p/>').text("Changing careers and enrolling in General Assembly's WDI course is one of the most challenging things  I've ever done. I'm gratefully for the opportunity to learn to become a life long student.")
 studentPersonal.attr('id', "studentPersonal")
 
 $('#teammate').hover(function() {
