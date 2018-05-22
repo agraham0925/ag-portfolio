cdb9c91837c1c25479b2a4d363bca6cc5ca64616
agraham <agraham0925@gmail.com>
2018-05-22 15:17:09 -0500

    all three items reflect different text on hover

--- js/app.js
+++ js/app.js
@@ -1,26 +1,43 @@
 console.log("js file connected");
 
-let teamPersonal = $('<p/>').text("PERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let teamPersonal = $('<p/>').text("TERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
 teamPersonal.attr('id', "teamPersonal");
 
-$('#teammate').hover(
-		function() {
-		console.log(this)
+let commPersonal = $('<p/>').text("CERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+commPersonal.attr('id', "commPersonal")
+
+let studentPersonal = $('<p/>').text("SERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+studentPersonal.attr('id', "studentPersonal")
+
+$('#teammate').hover(function() {
 
 		$('#teamOriginal').hide();
 		$('#teammate').append(teamPersonal);
 
-
-		// newText.appendTo('p');
-
-		//on hover hide my text and replace with new text and background
-		// on remove hover, revert to original
+	}, function(){
+		teamPersonal.remove();
+		$('#teamOriginal').show();
 	}
 );
 
+$('#communicator').hover(function() {
 
-// $("p").hover(function(){
-//     $(this).css("background-color", "yellow");
-//     }, function(){
-//     $(this).css("background-color", "pink");
-// });
+		$('#commOriginal').hide();
+		$('#communicator').append(commPersonal);
+
+	}, function(){
+		commPersonal.remove();
+		$('#commOriginal').show();
+	}
+);
+
+$('#student').hover(function() {
+
+		$('#studentOriginal').hide();
+		$('#student').append(studentPersonal);
+
+	}, function(){
+		studentPersonal.remove();
+		$('#studentOriginal').show();
+	}
+);
