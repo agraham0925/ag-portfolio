6769e0f135f9fd86f0e0c01bcda0a5e3743608df
agraham <agraham0925@gmail.com>
2018-05-24 15:24:29 -0500

    updated files

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 console.log("js file connected");
 
-let teamPersonal = $('<p/>').text("TERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let teamPersonal = $('<p/>').text("Torem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
 teamPersonal.attr({
 	id: "teamPersonal",
 	class: "statement-p"
 });
 
-let commPersonal = $('<p/>').text("CERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let commPersonal = $('<p/>').text("Corem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
 commPersonal.attr({
 	id: "commPersonal",
 	class: "statement-p"
 });
 
-let studentPersonal = $('<p/>').text("SERSONALpsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
+let studentPersonal = $('<p/>').text("Sorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim")
 studentPersonal.attr({
 	id: "studentPersonal",
 	class: "statement-p"
